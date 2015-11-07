f7fe9e21c1643927e1e01ac7ff8385589df35913
Chad Brewbaker <crb002@gmail.com>
2015-11-06 22:26:16 -0600

    cleanup

--- scala/Three.scala
+++ scala/Three.scala
@@ -1,11 +1,3 @@
-//main = do
-//       putStrLn "What is the quote?"
-//       quote <- getLine
-//       putStrLn "Who said it?"
-//       author <- getLine
-///\       putStrLn $ author ++ " says, \"" ++ quote ++ "\""
-//../haskell/Three.hs (END) 
-
 object Three {
   def main(args: Array[String]){  
     println("What is the quote?");
